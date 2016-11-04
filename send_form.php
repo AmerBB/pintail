e61815e0dfacd6c284bf73af01722eb7c439a909
Web <Web>
2016-11-04 00:17:34 +0000

    first commit

---
+++ send_form.php
@@ -0,0 +1,267 @@
+<?php
+
+if( isset($_POST['name']) && !empty($_POST['mail_corresponding']) ){  
+
+  $message = '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>pintail</title>
+<style type="text/css">
+
+/* GLOBAL */
+
+  * { -webkit-text-size-adjust:100%; -ms-text-size-adjust:100%;}
+  body { width:100%; margin:0px; padding:0px; }
+  img { outline:none; text-decoration:none; -ms-interpolation-mode:bicubic; }
+  a img { border:none; }
+  
+/*HIDING BLOCK*/
+
+  td[class=hide],tr[class=hide] {
+  max-height:0px !important;
+  font-size:0px !important;
+  display:none;
+  float:left;
+  overflow:hidden;
+  height:0px;
+  }
+    .d_n{
+    }.rsz_320{
+    }
+/* MEDIA QUERIES */ 
+
+  @media only screen and (max-width:480px)
+  {
+/* HIDING BLOCK */
+
+  tr[class=hide],td[class=hide] {
+   
+  max-height: 1000px !important;
+  height: auto !important;
+  font-size: auto !important;
+  display:block !important;
+  float:none !important;
+  overflow:visible !important;
+    }
+    
+/* Resizing full Tables, TDs, and IMGs*/
+
+  table[class=rsz_320],td[class=rsz_320],img[class=rsz_320]{
+    width:320px !important;
+    margin:0 auto !important;
+  }
+  
+/* BLOCK DISPLAYING */
+
+  table[class=d_b],
+  td[class=d_b],
+  img[class=d_b],
+  br[class=d_b],
+  span[class=d_b],
+  a[class=d_b],
+  table[id=d_b],
+  td[id=d_b],
+  img[id=d_b],
+  br[id=d_b],
+  span[id=d_b],
+  a[id=d_b]{
+  display:block !important;
+  width:100% !important;
+  } 
+  
+  /* NONE DISPLAYING */
+  table[class=d_n],
+  td[class=d_n],tr[class=d_n],
+  img[class=d_n],
+  br[class=d_n],
+  span[class=d_n],
+  a[class=d_n],
+  table[id=d_n],
+  td[id=d_n],tr[id=d_n],
+  img[id=d_n],
+  br[id=d_n],
+  span[id=d_n],
+  a[id=d_n]{
+  display:none !important;
+  }
+  td[class=ajuster]{padding-right:10px;}
+  }
+</style>
+</head>
+
+<body>
+<table width="100%" border="0" cellspacing="0" cellpadding="0">
+  <tr>
+    <td align="center"><table width="650" border="0" align="center" cellpadding="0" cellspacing="0" class="rsz_320" style="font-size:0px;">
+      <tr>
+        <td bgcolor="#6cc8e9">               <!--[if (gte mso 9)]> 
+        
+                <table width="100%" border="0" cellspacing="0" cellpadding="0">
+          <tr>
+            <td width="142"> <![endif]--> <table width="142" border="0" align="left" cellpadding="0" cellspacing="0" class="rsz_320">
+              <tr>
+                <td align="center"><img src="http://www.exedigit.fr/adsee/Gamned/2016/06/OP59/email/images/img_email_pintail_d_01.jpg" border="0" style="display:block" width="142" height="85" alt="pintail*"></td>
+              </tr>
+            </table>    <!--[if (gte mso 9)]> </td>
+            <td width="497"> <![endif]--> <table width="495" border="0" align="left" cellpadding="0" cellspacing="0" class="rsz_320">
+  <tr>
+    <td height="46"   align="center" valign="bottom"><strong><font face="verdana, Helvetica, sans-serif" color="#483486" style="font-size:16px;">Pintail - La n&eacute;o-banque pour la diaspora marocaine !</font></strong>  
+     </td>
+  </tr>
+  <tr>
+    <td height="39"   align="center" valign="top" dir="rtl"><font face="Arial, Helvetica, sans-serif" dir="rtl"   color="#483486" style="font-size:16px;"> بنتيل - البنك الجديد لمغاربة المهجر</font></td>
+  </tr>
+            </table>  
+            <!--[if (gte mso 9)]> 
+</td>
+            <td width="11"> <![endif]--> <table width="11" border="0" align="left" cellpadding="0" cellspacing="0" class="d_n">
+              <tr>
+                <td height="15">&nbsp;</td>
+              </tr>
+            </table> <!--[if (gte mso 9)]> </td>
+          </tr>
+        </table> <![endif]--> </td>
+      </tr>
+      <tr>
+        <td> <!--[if (gte mso 9)]> <table width="100%" border="0" cellspacing="0" cellpadding="0">
+          <tr>
+            <td width="160"> <![endif]--> <table width="160" border="0" align="left" cellpadding="0" cellspacing="0" class="rsz_320">
+              <tr>
+                <td width="72"><img src="http://www.exedigit.fr/adsee/Gamned/2016/06/OP59/email/images/img_email_pintail_d_03.jpg" border="0" style="display:block" width="72" height="79" alt=""></td>
+                <td align="left" valign="middle"><font face="verdana, Helvetica, sans-serif" color="#6cc8e9" style="font-size:13px;">Plateforme <br class="d_n">
+                  s&eacute;curis&eacute;e<br>
+                  <span dir="rtl">أرضية آمنة</span></font></td>
+              </tr>
+            </table> <!--[if (gte mso 9)]> </td>
+            <td width="1"> <![endif]--><table width="1" border="0" align="left" cellpadding="0" cellspacing="0" class="d_n">
+              <tr>
+                <td><img src="http://www.exedigit.fr/adsee/Gamned/2016/06/OP59/email/images/img_email_pintail_d_05.jpg" border="0" style="display:block" width="1" height="79" alt=""></td>
+              </tr>
+            </table> <!--[if (gte mso 9)]> </td>
+            <td width="218"> <![endif]--> <table width="218" border="0" align="left" cellpadding="0" cellspacing="0" class="rsz_320">
+              <tr>
+                <td width="64"><img src="http://www.exedigit.fr/adsee/Gamned/2016/06/OP59/email/images/img_email_pintail_d_06.jpg" border="0" style="display:block" width="64" height="79" alt=""></td>
+                <td align="left" valign="middle"><font face="verdana, Helvetica, sans-serif" color="#6cc8e9" style="font-size:13px;">Cr&eacute;ation de compte<br class="d_n">
+                  en quelques minutes<br>
+                  <span dir="rtl">دقائق قليلة لإنشاء حسابكم</span></font></td>
+              </tr>
+            </table> <!--[if (gte mso 9)]> </td>
+            <td width="1"> <![endif]--> <table width="1" border="0" align="left" cellpadding="0" cellspacing="0" class="d_n">
+              <tr>
+                <td><img src="http://www.exedigit.fr/adsee/Gamned/2016/06/OP59/email/images/img_email_pintail_d_05.jpg" border="0" style="display:block" width="1" height="79" alt=""></td>
+              </tr>
+            </table> <!--[if (gte mso 9)]> </td>
+            <td width="270"> <![endif]--> <table width="268" border="0" align="left" cellpadding="0" cellspacing="0" class="rsz_320">
+              <tr>
+                <td width="70"><img src="http://www.exedigit.fr/adsee/Gamned/2016/06/OP59/email/images/img_email_pintail_d_08.jpg" border="0" style="display:block" width="70" height="79" alt=""></td>
+                <td align="left" valign="middle"><font face="verdana, Helvetica, sans-serif" color="#6cc8e9" style="font-size:13px;">Transfert d\'argent<br class="d_n">
+                  low-cost<br>
+                  <span dir="rtl">تحويل الأموال بتكلفة بسيطة</span></font></td>
+              </tr>
+          </table> <!--[if (gte mso 9)]> </td>
+          </tr>
+        </table> <![endif]--></td>
+      </tr>
+      <tr>
+        <td background="http://www.exedigit.fr/adsee/Gamned/2016/06/OP59/email/images/img_email_pintail_d_11_10.jpg" style="background-size:100% 100%;" bgcolor="#4d3189" width="650" height="220" valign="top">
+  <!--[if gte mso 9]>
+  <v:rect xmlns:v="urn:schemas-microsoft-com:vml" fill="true" stroke="false" style="width:650px;height:220px;">
+    <v:fill type="tile" src="images/img_email_pintail_d_11_10.jpg" color="#4d3189" />
+    <v:textbox inset="0,0,0,0">
+  <![endif]-->
+  <div><!--[if (gte mso 9)]> <table width="100%" border="0" cellspacing="0" cellpadding="0">
+      <tr>
+        <td width="422" valign="top"> <![endif]--><table width="422" border="0" align="left" cellpadding="0" cellspacing="0" class="rsz_320">
+          <tr>
+            <td width="21">&nbsp;</td>
+            <td height="24">&nbsp;</td>
+            </tr>
+          <tr>
+            <td width="21">&nbsp;</td>
+            <td align="left" class="ajuster"><font face="verdana, Helvetica, sans-serif" color="#ffffff" style="font-size:12px;">Salut, je viens de m\'inscrire pour tester Pintail avec toi, une nouvelle application de transferts low-costs pour la diaspora marocaine. Tu pourras la t&eacute;l&eacute;charger d&egrave;s septembre et tu recevras des instructions de l\'&eacute;quipe Pintail par email entretemps. A bientot!<br>
+              '.$_POST['firstname'].' '.$_POST['name'].'</font></td>
+            </tr>
+          <tr>
+            <td width="21">&nbsp;</td>
+            <td height="18"></td>
+            </tr>
+          <tr>
+            <td width="21">&nbsp;</td>
+            <td align="left" valign="top" class="ajuster"  dir="rtl"  style="text-align:left" ><font face="Arial, Helvetica, sans-serif" color="#ffffff" style="font-size:11px;" size="1"> أهلا بك، لقد قمت للتو بتسجيل نفسي لكي أشارك رفقتك في المرحلة التجريبية لبنتيل. بنتيل هو تطبيق جديد يمكن الجالية المغربية من تحويل الأموال نحو المغرب بتكلفة منخفضة. سيكون بإمكانك تحميل التطبيق ابتداء من شتنبر وستتوصل بالمعلومات اللازمة من بنتيل عبر الإيميل في حينه. إلى اللقاء <br>
+              '.$_POST['firstname'].' '.$_POST['name'].'</font></td>
+            </tr>
+        </table><!--[if (gte mso 9)]> </td>
+        <td> <![endif]--><table width="127" border="0" align="left" cellpadding="0" cellspacing="0" class="rsz_320">
+          <tr>
+            <td height="31">&nbsp;</td>
+          </tr>
+          <tr>
+            <td align="center"><img src="http://www.exedigit.fr/adsee/Gamned/2016/06/OP59/email/images/img_email_pintail_d_14.png" border="0" style="display:block" width="102" height="102" alt=""></td>
+          </tr>
+          <tr>
+            <td height="23">&nbsp;</td>
+          </tr>
+          <tr>
+            <td align="center"><a href="http://www.go-live-pintail.net/" target="_blank"><img src="http://www.exedigit.fr/adsee/Gamned/2016/06/OP59/email/images/img_email_pintail_d_18.png" border="0" style="display:block" width="177" height="37" alt="je m\'inscris !"></a></td>
+          </tr>
+          <tr>
+            <td height="20" align="center"></td>
+          </tr>
+        </table>
+        <!--[if (gte mso 9)]> </td>
+      </tr>
+    </table> <![endif]-->
+  </div>
+  <!--[if gte mso 9]>
+    </v:textbox>
+  </v:rect>
+  <![endif]-->
+</td>
+      </tr>
+      <tr>
+        <td bgcolor="#19386e"><table width="100%" border="0" cellpadding="0" cellspacing="0" class="rsz_320">
+          <tr>
+            <td width="99"><img src="http://www.exedigit.fr/adsee/Gamned/2016/06/OP59/email/images/img_email_pintail_d_11.jpg" border="0" style="display:block" width="99" height="68" alt="pintail"></td>
+            <td>&nbsp;</td>
+            <td width="222" valign="middle"><table width="222" border="0" cellspacing="0" cellpadding="0">
+              <tr>
+                <td align="left" valign="middle"><span class="ajuster"><font face="verdana, Helvetica, sans-serif" color="#ffffff" style="font-size:12px;">Suivez-nous :<br>
+                  <span dir="rtl">تابعونا</span></font></span></td>
+                <td width="30" valign="middle"><a href="https://twitter.com/PintailApp/" target="_blank"><img src="http://www.exedigit.fr/adsee/Gamned/2016/06/OP59/email/images/img_email_pintail_d_27.jpg" border="0" style="display:block" width="30" height="30" alt="twitter"></a></td>
+                <td width="9" valign="middle">&nbsp;</td>
+                <td width="30" valign="middle"><a href="http://www.viadeo.com/fr/company/indigodigital " target="_blank"><img src="http://www.exedigit.fr/adsee/Gamned/2016/06/OP59/email/images/img_email_pintail_d_29.jpg" border="0" style="display:block" width="30" height="30" alt="viadeo"></a></td>
+                <td width="8" valign="middle">&nbsp;</td>
+                <td width="30" valign="middle"><a href="https://www.linkedin.com/company/indigodigital-ag " target="_blank"><img src="http://www.exedigit.fr/adsee/Gamned/2016/06/OP59/email/images/img_email_pintail_d_31.jpg" border="0" style="display:block" width="31" height="30" alt="linkedin"></a></td>
+                <td width="20" valign="middle">&nbsp;</td>
+              </tr>
+            </table></td>
+          </tr>
+        </table></td>
+      </tr>
+      <tr>
+        <td height="30">&nbsp;</td>
+      </tr>
+    </table></td>
+  </tr>
+</table>
+
+</body>
+</html>';
+  $to = $_POST['mail_corresponding']; // this is your Email address
+  $from = $_POST['mail']; // this is the sender's Email address  
+  $subject = $_POST['firstname']." ".$_POST['name']." te propose de découvrir Pintail – la néo-banque pour la diaspora marocaine !" ";
+  $headers = "From:" . $from;
+  $headers .= "From: no-reply@pintail.com";
+  $headers .= "MIME-version: 1.0\n";
+  $headers .= "Content-type: text/html; charset=utf-8\n";
+
+  if( mail($to,$subject,$message,$headers) ){
+    echo "Sent";
+  }else{
+    echo "NotSent";
+  } 
+}
+
+
+?>
